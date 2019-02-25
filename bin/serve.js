484890e39031140eeb8327cb040ae45468a34d55
Andreas Bomholtz <andreas@seluxit.com>
2019-02-25 12:19:34 +0100

    Now test is parsing

---
+++ bin/serve.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+const zlib = require('zlib');
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const Wapp = require('../lib/wapp');
+const tui = require('../lib/tui');
+
+const app = express();
+const wapp = new Wapp();
+
+const port = 3000;
+const HOST = process.env.WAPPSTO_HOST || 'wappsto.com';
+let sessionID = '';
+
+app.use('/services', proxy({
+    target: `https://${HOST}`,
+    changeOrigin: true,
+    ws: true,
+    logLevel: 'debug',
+}));
+
+app.use('/wapp-api.js', proxy({
+    target: `https://light.${HOST}`,
+    changeOrigin: true,
+    logLevel: 'debug',
+    onProxyRes: (proxyRes, req, res) => {
+        let originalBody = Buffer.from([]);
+        proxyRes.on('data', (data) => {
+            originalBody = Buffer.concat([originalBody, data]);
+        });
+
+        proxyRes.on('end', () => {
+            const bodyString = zlib.gunzipSync(originalBody).toString('utf8');
+            const newBody = `window.sessionID = '${sessionID}';\n${bodyString}`;
+
+            res.set({
+                'content-type': 'text/html; charset=utf-8',
+                'content-encoding': 'gzip',
+            });
+            res.write(zlib.gzipSync(newBody));
+            res.end();
+        });
+    },
+    selfHandleResponse: true,
+}));
+
+app.use(express.static('foreground'));
+
+app.listen(port, () => console.log(`Wapp is running on port ${port}!`));
+
+const run = async () => {
+    try {
+        await wapp.init();
+        sessionID = await wapp.getInstallationSession();
+    } catch (err) {
+        if (err.message === 'LoginError') {
+            tui.showError('Failed to Login, please try again.');
+        } else {
+            console.log(err);
+            console.log('Run error');
+        }
+        process.exit(-1);
+    }
+};
+run();
--- test/wapp.test.js
+++ test/wapp.test.js
@@ -12,12 +12,54 @@ const Wapp = require('../lib/wapp');
 // This sets the mock adapter on the default instance
 const mock = new MockAdapter(axios);
 
-mock.onPatch('https://wappsto.com/services/version/version_id').reply(200, [])
+mock.onPatch('https://wappsto.com/services/version/version_id').reply(200, {
+    file: [{
+        meta: {
+            id: 'file_id',
+        },
+    }],
+})
     .onDelete('https://wappsto.com/services/application/application_id').reply(200, [])
     .onDelete('https://wappsto.com/services/version/version_id')
     .reply(200, [])
     .onDelete('https://wappsto.com/services/installation?this_version_id=version_id')
-    .reply(200, []);
+    .reply(200, [])
+    .onPut('https://wappsto.com/files/version/version_id')
+    .reply(201, {})
+    .onGet('https://wappsto.com/services/application/application_id?expand=2&verbose=true')
+    .reply(200, {
+        meta: {
+            id: 'application_id',
+        },
+        version: [{
+            meta: {
+                id: 'version_id',
+            },
+            file: [{
+                meta: {
+                    id: 'file_id',
+                },
+            }],
+        }],
+    })
+    .onGet('https://wappsto.com/services/application?expand=2&verbose=true')
+    .reply(200, [])
+    .onPost('https://wappsto.com/services/application')
+    .reply(201, {
+        meta: {
+            id: 'application_id',
+        },
+        version: [{
+            meta: {
+                id: 'version_id',
+            },
+            name: 'Wapp Test',
+            file: [],
+        }],
+    })
+    .onGet('https://wappsto.com/files/file/file_id')
+    .reply(200, 'file content');
+
 
 const conf = new Configstore(pkg.name);
 conf.delete('session');
@@ -32,9 +74,29 @@ test('Login Fail', async (t) => {
     const wapp = new Wapp();
     mock.onPost('https://wappsto.com/services/session').replyOnce(401);
     mockInquirer([{
+        username: '',
+        password: '',
+    }, {
+        username: 'user',
+        password: '',
+    }, {
         username: 'user@wappsto.com',
         password: 'password',
     }]);
+
+    try {
+        await wapp.init();
+        t.fail();
+    } catch (err) {
+        t.is(err.message, 'Validation failed for field username');
+    }
+
+    try {
+        await wapp.init();
+        t.fail();
+    } catch (err) {
+        t.is(err.message, 'Validation failed for field password');
+    }
 
     try {
         await wapp.init();
@@ -87,7 +149,10 @@ test('create new empty wapp', async (t) => {
                 name: 'Wapp Test',
                 file: [],
             }],
-        });
+        })
+        .onGet('https://wappsto.com/files/file/file_id')
+        .reply(200, 'file content');
+
     mockInquirer([{
         name: 'Test Wapp',
         features: ['foreground'],
@@ -96,11 +161,15 @@ test('create new empty wapp', async (t) => {
 
     await wapp.create();
 
+    t.true(files.fileExists('.application'));
+    t.true(files.fileExists('manifest.json'));
     t.true(files.directoryExists('foreground'));
     t.false(files.directoryExists('background'));
+    t.false(files.fileExists('foreground/index.html'));
 });
 
-test('update files', async (t) => {
+
+test('update empty files', async (t) => {
     const wapp = new Wapp();
 
     mock.onGet('https://wappsto.com/services/application/application_id').replyOnce(200, {
@@ -114,11 +183,74 @@ test('update files', async (t) => {
             name: 'Wapp Test',
             file: [],
         }],
+    }).onAny().reply((opt) => {
+        t.log(opt);
+        return [500, {}];
     });
 
     const updatedFiles = await wapp.update();
 
     t.deepEqual(updatedFiles, []);
+});
+
+test('create new example wapp', async (t) => {
+    const wapp = new Wapp();
+
+    mockInquirer([{
+        name: 'Test Wapp',
+        features: ['foreground', 'background'],
+        examples: true,
+    }]);
+
+    await wapp.create();
+
+    t.true(files.fileExists('.application'));
+    t.true(files.fileExists('manifest.json'));
+    t.true(files.directoryExists('foreground'));
+    t.true(files.directoryExists('background'));
+    t.true(files.fileExists('foreground/index.html'));
+    t.true(files.fileExists('background/package.json'));
+});
+
+test('update test files', async (t) => {
+    const wapp = new Wapp();
+
+    mock.onGet('https://wappsto.com/services/application/application_id').replyOnce(200, {
+        meta: {
+            id: 'application_id',
+        },
+        version: [{
+            meta: {
+                id: 'version_id',
+            },
+            name: 'Wapp Test',
+            file: [],
+        }],
+    }).onAny().reply((opt) => {
+        t.log(opt);
+        return [500, {}];
+    });
+
+    const updatedFiles = await wapp.update();
+    t.log(updatedFiles);
+    t.deepEqual(updatedFiles, [
+        {
+            name: 'foreground/index.html',
+            status: 'created',
+        },
+        {
+            name: 'foreground/main.js',
+            status: 'created',
+        },
+        {
+            name: 'background/main.js',
+            status: 'created',
+        },
+        {
+            name: 'background/package.json',
+            status: 'created',
+        },
+    ]);
 });
 
 test('delete wapp', async (t) => {
