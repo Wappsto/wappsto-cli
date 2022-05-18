1ebea28cc38400caec7ef544323bd53c9a414f07
Andreas Bomholtz <andreas@seluxit.com>
2022-05-18 12:14:29 +0200

    Make sure that background is not stopping

--- examples/bare/background/main.js
+++ examples/bare/background/main.js
@@ -2,4 +2,8 @@ const Wappsto = require('wappsto-wapp');
 
 Wappsto.startLogging();
 
-console.log('Background wapp started and stopped');
+console.log('Background wapp started.');
+
+setInterval(() => {
+  console.log('Background wapp is alive.');
+}, 5000);
