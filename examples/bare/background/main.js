ece383740f2125c73350b74eae315d854371fae1
Andreas Bomholtz <andreas@seluxit.com>
2023-02-10 10:29:07 +0100

    Linting examples

--- examples/bare/background/main.js
+++ examples/bare/background/main.js
@@ -5,3 +5,7 @@ console.log('Background wapp started.');
 setInterval(() => {
   console.log('Background wapp is alive.');
 }, 5000);
+
+Wappsto.wappStorage().then(() => {
+  console.log('Storage created');
+});
